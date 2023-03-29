549158e598149760b78fa1a68d55426ce50bd23a
Tomas Salgado <tomassalgadodev@gmail.com>
2023-03-29 11:16:47 -0600

    refactor: scrapeGetAlbumDirect function now grabs a different endpoint from Spotify API with more album data

--- scrapers.js
+++ scrapers.js
@@ -296,7 +296,7 @@ async function scrapeGetAlbumDirect(albumID) {
 
     page.on('response', async (response) => {
         const request = response.request();
-        if (request.url().includes('https://api-partner.spotify.com/pathfinder/v1/query?operationName=queryAlbumTracks&variables') && request.method() === 'GET') {
+        if (request.url().includes('https://api-partner.spotify.com/pathfinder/v1/query?operationName=getAlbumMetadata&variables') && request.method() === 'GET') {
             data = await response.json();
         }
     });
@@ -312,7 +312,7 @@ async function scrapeGetAlbumDirect(albumID) {
     // scrapeSearchArtist('kublai');
     // scrapeSearchArtistDirect('taylor swift');
     // scrapeGetArtistDirect('5BIOo2mCAokFcLHXO2Llb4');
-    scrapeGetAlbumDirect('1xfiE1XllZeRL2LT7zB7Ns');
+    // scrapeGetAlbumDirect('1xfiE1XllZeRL2LT7zB7Ns');
     // scrapeGetAlbums('https://open.spotify.com/artist/5BIOo2mCAokFcLHXO2Llb4');
     // scrapeGetArtist('3LZZPxNDGDFVSIPqf4JuEf');
     // scrapeSearchArtist2('joker');
