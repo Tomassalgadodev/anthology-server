5a2e757abff6092f5edd7bfc234d6d1f1b236ece
Tomas Salgado <tomassalgadodev@gmail.com>
2023-05-29 17:58:36 -0600

    Updated db variables to the heroku db

--- database.js
+++ database.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql2');
 
 const database = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'guitar221',
-    database: 'no_skips_db'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b8c2ed6a2e4d43',
+    password: 'c8f89cdd',
+    database: 'heroku_76cbec24ca88224'
 }).promise();
 
 module.exports = database;
+
+// mysql://b8c2ed6a2e4d43:c8f89cdd@us-cdbr-east-06.cleardb.net/heroku_76cbec24ca88224?reconnect=true
+
+// const database = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'guitar221',
+//     database: 'no_skips_db'
+// }).promise();
